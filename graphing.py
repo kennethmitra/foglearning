778821301d197ae72f619673964c60f9b8967bb0
Kenneth Mitra <kennethmitra@gmail.com>
2022-11-13 17:47:06 -0600

    Added bar chart

--- graphing.py
+++ graphing.py
@@ -6,9 +6,60 @@ from glob import glob
 import re
 import matplotlib.pyplot as plt
 
-EXPERIMENT_TITLE = "MNIST Varying num_share_devices"
+####################
+# MNIST Line graph
+####################
+# EXPERIMENT_TITLE = "MNIST Varying num_share_devices"
+# CSVS_DIR = "results/mnist_num_share_exp/acc"
+# FIG_OUT_DIR = "results/figures"
+#
+# data = pd.DataFrame()
+# for fname in glob(f"{CSVS_DIR}/*.csv"):
+#     print(fname)
+#     # Extract parameter value
+#     m = re.search(r"share_(\d+)_of_30-tag-Accuracy_avg_acc.csv", fname)
+#     if m:
+#         # Parse experiment
+#         parameter_val = int(m.group(1))
+#         if parameter_val in (0, 1, 14, 29):  # SELECT certain param values cause they overlap too much
+#             # Parse csv to get desired timeseries
+#             df = pd.read_csv(fname, index_col=False)
+#             df.drop(columns='Wall time', inplace=True)
+#             df.reset_index(drop=True, inplace=True)
+#             df['num_share_devices'] = parameter_val
+#             df['type'] = 'decentralized'
+#             data = pd.concat([data, df])
+#         else:
+#             continue
+#     else:
+#         # Parse control
+#         df = pd.read_csv(fname, index_col=False)
+#         df.drop(columns='Wall time', inplace=True)
+#         df.reset_index(drop=True, inplace=True)
+#         df['num_share_devices'] = 'fedavg'
+#         df['type'] = 'fedavg'
+#         data = pd.concat([data, df])
+#
+# sns.set(rc={'figure.figsize':(6, 5)})
+# # Put the params here too
+# g = sns.lineplot(data=data, x="Step", y="Value", hue="num_share_devices", errorbar=("se", 2), hue_order=(0, 1, 14, 29, 'fedavg'))
+# # g.set(xscale='log', yscale='log')
+# plt.title(EXPERIMENT_TITLE)
+# plt.ylabel("Test Accuracy")
+#
+# Path(f"{FIG_OUT_DIR}").mkdir(parents=True, exist_ok=True)
+# plt.savefig(f"{FIG_OUT_DIR}/{EXPERIMENT_TITLE}")
+# plt.show()
+
+####################
+# MNIST Bar graph
+####################
+max_step = 10
+EXPERIMENT_TITLE = f"MNIST Avg accuracy over steps 0 through {max_step}"
 CSVS_DIR = "results/mnist_num_share_exp/acc"
 FIG_OUT_DIR = "results/figures"
+
+
 
 data = pd.DataFrame()
 for fname in glob(f"{CSVS_DIR}/*.csv"):
@@ -18,11 +69,12 @@ for fname in glob(f"{CSVS_DIR}/*.csv"):
     if m:
         # Parse experiment
         parameter_val = int(m.group(1))
-        if parameter_val in (0, 1, 14, 29):
+        if parameter_val in (0, 1, 2, 5, 14, 29):  # SELECT certain param values cause they overlap too much
             # Parse csv to get desired timeseries
-            df = pd.read_csv(fname, index_col=False)
-            df.drop(columns='Wall time', inplace=True)
-            df.reset_index(drop=True, inplace=True)
+            log = pd.read_csv(fname, index_col=False)
+            log.drop(columns='Wall time', inplace=True)
+            log.reset_index(drop=True, inplace=True)
+            df = pd.DataFrame(log.query(f"Step < {max_step}").mean()).transpose()
             df['num_share_devices'] = parameter_val
             df['type'] = 'decentralized'
             data = pd.concat([data, df])
@@ -30,24 +82,30 @@ for fname in glob(f"{CSVS_DIR}/*.csv"):
             continue
     else:
         # Parse control
-        df = pd.read_csv(fname, index_col=False)
-        df.drop(columns='Wall time', inplace=True)
-        df.reset_index(drop=True, inplace=True)
+        log = pd.read_csv(fname, index_col=False)
+        log.drop(columns='Wall time', inplace=True)
+        log.reset_index(drop=True, inplace=True)
+        df = pd.DataFrame(log.query(f"Step < {max_step}").mean()).transpose()
         df['num_share_devices'] = 'fedavg'
         df['type'] = 'fedavg'
         data = pd.concat([data, df])
 
+data.reset_index(drop=True, inplace=True)
 sns.set(rc={'figure.figsize':(6, 5)})
-g = sns.lineplot(data=data, x="Step", y="Value", hue="num_share_devices", errorbar=("se", 2), hue_order=(0, 1, 14, 29, 'fedavg'))
-# g.set(xscale='log', yscale='log')
+# Put the params here too
+g = sns.barplot(data=data, x="num_share_devices", y="Value", errorbar=("se", 2), order=(0, 1, 2, 5, 14, 29, 'fedavg'))
 plt.title(EXPERIMENT_TITLE)
 plt.ylabel("Test Accuracy")
+plt.ylim(0.81, 0.86)
 
 Path(f"{FIG_OUT_DIR}").mkdir(parents=True, exist_ok=True)
 plt.savefig(f"{FIG_OUT_DIR}/{EXPERIMENT_TITLE}")
 plt.show()
 
 
+####################
+# CIFAR10 Line graphs
+####################
 # EXPERIMENT_TITLE = "CIFAR10 Varying num_share_devices"
 # CSVS_DIR = "results/cifar_num_share_exp"
 # FIG_OUT_DIR = "results/figures"
